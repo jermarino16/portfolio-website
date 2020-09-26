9d50c7f394534f9af0a7b78cad7df106389f203b
Jeremy Marino <jermarino16@gmail.com>
2020-09-26 16:58:51 -0700

    styling header and will have it use state

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,32 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+// import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 
 const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: "auto",
+  container: {
+    position: "absolute",
+    width: "80vw",
+    height: "100vh",
+    zIndex: "1",
+    backgroundColor: "orangered",
   },
 });
 
 export default function Sidebar(props) {
   const classes = useStyles();
-  const [showSidebar, setShowSidebar] = useState(false);
+  // const [showSidebar, setShowSidebar] = useState(false);
 
   return (
     <div>
       <React.Fragment>
-        {props.show}
-        <SwipeableDrawer
-          anchor={anchor}
-          open={state[anchor]}
-          onClose={toggleDrawer(anchor, false)}
-          onOpen={toggleDrawer(anchor, true)}
-        >
-          {list(anchor)}
-        </SwipeableDrawer>
+        {props.show && (
+          <div className={classes.container}>testestsetestsetsetset</div>
+        )}
       </React.Fragment>
     </div>
   );
